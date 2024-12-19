ae445204d71e1b6aa995619d6e6361054cb819e1
Tom Ward <wardusmcbay@gmail.com>
2024-12-19 14:33:13 +0000

    init commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const profilesRouter = require('./controllers/profiles');
+
+const testJWTRouter = require('./controllers/test-jwt');
+
+const usersRouter = require('./controllers/users');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.json());
+
+// Routes go here
+app.use('/test-jwt', testJWTRouter);
+
+app.use('/users', usersRouter);
+
+app.use('/profiles', profilesRouter);
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
